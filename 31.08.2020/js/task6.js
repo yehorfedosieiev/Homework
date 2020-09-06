904173f4b6df6e6643a8ed4f71117b741084d63f
Yehor Fedosieiev <pifagorka@gmail.com>
2020-09-06 22:08:05 +0300

    31.08.2020 fixing task6

--- 31.08.2020/js/task6.js
+++ 31.08.2020/js/task6.js
@@ -6,7 +6,7 @@ let elementObj = {
     img:{
         src: "https://www.google.com.ua/logos/doodles/2017/bella-akhmadulinas-80th-birthday-5134676388741120.3-law.gif",
         alt: "",
-        style: "border: 1px solid #ccc",
+        style: "border: 1px solid #ccc; display: block; margin-left: auto; margin-right: auto",
         width: "200"
     }
 }
